a61f0e3a7a59a91456fc39a9a1a438c0d09c5d7a
Nick <nbadger@mintel.com>
2021-06-24 15:00:24 +0100

    Revert "tanka: Don't render local env by default (#21)"
    This reverts commit eea0097a4d8dade65922c2c1b9b9bfe86ffbb920.

--- modules/tanka/generate.sh
+++ modules/tanka/generate.sh
@@ -1,40 +1,45 @@
 #!/bin/bash
 
 set -e
-shopt -s extglob; # Need extented glob patterns to exclude local env by default.
 
 APP="$1"
-if [[ -z "$APP" ]]; then
-  APP="*"
-fi
-
 ENV="$2"
-if [[ -z "$ENV" ]]; then
-  # Don't render the local env unless explicitly specified.
-  ENV="!(local)"
-fi
 
-TANKA_DEFAULT_EXPORT_FMT="{{.apiVersion}}.{{.kind}}-{{ if.metadata.namespace}}{{.metadata.namespace }}-{{end}}{{.metadata.name }}"
-TANKA_EXPORT_FMT=${TANKA_EXPORT_FMT:-$TANKA_DEFAULT_EXPORT_FMT}
+TANKA_EXPORT_FMT="{{.apiVersion}}.{{.kind}}-{{ if.metadata.namespace}}{{.metadata.namespace }}-{{end}}{{.metadata.name }}"
+TANKA_REPO_DIR=$(pwd)
+ALL_ENVS=$(find environments -type f -name main.jsonnet -printf '%h\n' | grep "$APP" | grep "$ENV")
 
 echo
 echo "Generating rendered manifests in ./rendered"
 echo "Run 'make jsonnet/[install|update]' beforehand if you require vendor package installation"
 echo
 
-mkdir -p "rendered"
-touch "rendered/.gitkeep"
+mkdir -p "$(pwd)/rendered/"
+touch "$(pwd)/rendered/.gitkeep"
 
 # Generate the list of directories to render
 
-for main_file in environments/$APP/$ENV/main.jsonnet; do
-  env_dir="$(dirname "$main_file")"
-  rendered_dir="rendered/$env_dir"
-  echo "Generating $rendered_dir"
-  mkdir -p "$rendered_dir"
-  touch "$rendered_dir/kustomization.yaml"
-  yq -i eval 'del(.resources)' "$rendered_dir/kustomization.yaml"
-  rm -rf "$rendered_dir/manifests"
-  tk export --format="$TANKA_EXPORT_FMT" "$env_dir" "$rendered_dir/manifests" > /dev/null
-  (cd "$rendered_dir" && kustomize edit add resource ./manifests/*.yaml)
+if [[ -n "$APP" ]]; then
+  dirs=$(echo "$ALL_ENVS" | grep "$APP")
+  if [[ -n "$ENV" ]]; then
+    dirs=$(echo "$dirs" | grep "$ENV")
+  fi
+else
+  if [[ -n "$ENV" ]]; then
+    dirs=$(echo "$ALL_ENVS" | grep "$ENV")
+  else
+    dirs="$ALL_ENVS"
+  fi
+fi
+
+for env_path in $dirs; do
+  echo "Generating $env_path"
+  mkdir -p "./rendered/$env_path"
+  pushd "./rendered/$env_path" > /dev/null || exit 1
+  touch kustomization.yaml
+  yq -i eval 'del(.resources)' kustomization.yaml
+  rm -rf ./manifests/*.yaml
+  tk export --format="$TANKA_EXPORT_FMT" "$TANKA_REPO_DIR/$env_path" ./manifests > /dev/null
+  kustomize edit add resource ./manifests/*.yaml
+  popd > /dev/null || exit 1
 done
