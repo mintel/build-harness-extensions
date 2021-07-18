3f04dad5432a58a7179274b57182f749e8fdd88d
Nick <nbadger@mintel.com>
2021-07-18 18:56:34 +0100

    shellcheck fixes

--- modules/k3d/k3d.sh
+++ modules/k3d/k3d.sh
@@ -14,6 +14,7 @@ K3D_K8S_IMAGE=${K3D_K8S_IMAGE:-"rancher/k3s:v1.20.4-k3s1"}
 K3D_CLUSTER_NAME=${K3D_CLUSTER_NAME:-"local"}
 K3D_DOCKER_REGISTRY_NAME=${K3D_DOCKER_REGISTRY_NAME:-"registry.localhost"}
 K3D_DOCKER_REGISTRY_PORT=${K3D_DOCKER_REGISTRY_PORT:-5000}
+K3D_INSTALL_DOCKER_REGISTRY=${K3D_INSTALL_DOCKER_REGISTRY:-"0"}
 K3D_WAIT=${K3D_WAIT:-"120s"}
 K3D_API_SERVER_ADDRESS=${K3D_API_SERVER_ADDRESS:-"0.0.0.0"}
 K3D_API_SERVER_PORT=${K3D_API_SERVER_PORT:-6443}
@@ -21,19 +22,19 @@ K3D_API_SERVER_PORT=${K3D_API_SERVER_PORT:-6443}
 ## Create a cluster with the local registry enabled in container
 create() {
 
-   if [ "$(k3d cluster list | grep -o ${K3D_CLUSTER_NAME})" ]; then
-     echo "K3d cluster ${K3D_CLUSTER_NAME} already exists - you may want to cleanup with: make k3d/delete"
-     exit 1
-   fi
+  if [ "$(k3d cluster list | grep -o "${K3D_CLUSTER_NAME}")" ]; then
+    echo "K3d cluster ${K3D_CLUSTER_NAME} already exists - you may want to cleanup with: make k3d/delete"
+    exit 1
+  fi
 
-   if [ ! "$(k3d registry list | grep -o ${K3D_DOCKER_REGISTRY_NAME})" ]; then
-    k3d registry create ${K3D_DOCKER_REGISTRY_NAME} --port "${K3D_DOCKER_REGISTRY_PORT}"
+  if [ "${K3D_INSTALL_DOCKER_REGISTRY}" = '1' ] && [ ! "$(k3d registry list | grep -o "${K3D_DOCKER_REGISTRY_NAME}")" ]; then
+    k3d registry create "${K3D_DOCKER_REGISTRY_NAME}" --port "${K3D_DOCKER_REGISTRY_PORT}"
   fi
 
   k3d cluster create "${K3D_CLUSTER_NAME}" \
     --image="${K3D_K8S_IMAGE}" \
     --api-port="${K3D_API_SERVER_ADDRESS}:${K3D_API_SERVER_PORT}" \
-    --timeout=${K3D_WAIT} \
+    --timeout="${K3D_WAIT}" \
     --registry-create=false \
     --registry-use="${K3D_DOCKER_REGISTRY_NAME}:${K3D_DOCKER_REGISTRY_PORT}"
 }
