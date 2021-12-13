704669e022a985b3dd2ad3e0898e47119c5996ad
Barry Tucker <btucker@mintel.com>
2021-12-13 16:19:29 +0000

    Adding function to update helm chart in repo

---
+++ modules/k8s/tanka/update-helm-chart.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+CHART_NAME="$1"
+
+helm repo list | grep -i mintel > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  helm repo add mintel https://mintel.gihub.io/helm-charts > /dev/null 2>&1
+fi
+
+helm repo update > /dev/null 2>&1
+
+result=$(helm search repo ${CHART_NAME})
+
+latest_version=`echo $result | sed "s|.*${CHART_NAME} \([0-9\.]*\) .*|\1|"`
+
+sed -i "\|.*${CHART_NAME}.*|,+1 d" chartfile.yaml
+
+tk tool charts add $1@$latest_version
