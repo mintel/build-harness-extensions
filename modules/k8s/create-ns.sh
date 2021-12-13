20b10ceee16fbf9dece9b69b8bcc61306a1e0cd6
Barry Tucker <btucker@mintel.com>
2021-12-13 12:37:22 +0000

    Avoid hardcoding cluster name

--- modules/k8s/create-ns.sh
+++ modules/k8s/create-ns.sh
@@ -7,7 +7,7 @@ if [ "$(k3d cluster list | grep -o -E "^${K3D_CLUSTER_NAME}")" ]; then
   namespaces=$(tk env list --json | jq -r '.[].spec.namespace' | sort | uniq)
   while IFS= read -r namespace; do
     echo "Creating namespace $namespace ....."
-    out=$(kubectl --context k3d-local create namespace $namespace 2>&1)
+    out=$(kubectl --context ${K3D_CLUSTER_NAME} create namespace $namespace 2>&1)
     if  [[ "$out" == *"AlreadyExists"* ]]; then
       echo "Namespace $namespace already exists, skipping"
     else
