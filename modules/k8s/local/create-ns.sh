09dcb99048b4bab3048f7a1916db25e2839bdfc4
Nick <>
2022-10-31 09:33:40 +0000

    Remove unwanted debug

--- modules/k8s/local/create-ns.sh
+++ modules/k8s/local/create-ns.sh
@@ -4,7 +4,6 @@ K3D_CLUSTER_NAME=${K3D_CLUSTER_NAME:-"local"}
 
 if [ "$(k3d cluster list | grep -o -E "^${K3D_CLUSTER_NAME}")" ]; then
   set +e
-  tk env list --json
   namespaces=$(tk env list --json | jq -r '.[].spec.namespace' | sort | uniq)
   while IFS= read -r namespace; do
     echo "Creating namespace $namespace ....."
