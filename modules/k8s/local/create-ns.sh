4684e288e233afed8b9bad96654a1182adee3afe
Jaye Doepke <jdoepke@mintel.com>
2023-07-26 13:11:00 -0500

    shellcheck

--- modules/k8s/local/create-ns.sh
+++ modules/k8s/local/create-ns.sh
@@ -2,12 +2,12 @@
 
 K3D_CLUSTER_NAME=${K3D_CLUSTER_NAME:-"local"}
 
-if [ "$(k3d cluster list | grep -o -E "^${K3D_CLUSTER_NAME}")" ]; then
+if k3d cluster list | grep -q -o -E "^${K3D_CLUSTER_NAME}"; then
   set +e
-  namespaces=$(find rendered -type f -path '*/manifests/*.yaml' -print0 | xargs -0 -P1 yq eval-all --output-format csv '[.metadata.namespace] | map(select(. != null))' | tr ',' '\n' | sort -u)
+  namespaces="$(find rendered -type f -path '*/manifests/*.yaml' -print0 | xargs -0 -P1 yq eval-all --output-format csv '[.metadata.namespace] | map(select(. != null))' | tr ',' '\n' | sort -u)"
   while IFS= read -r namespace; do
     echo "Creating namespace $namespace ....."
-    out=$(kubectl --context k3d-${K3D_CLUSTER_NAME} create namespace $namespace 2>&1)
+    out=$(kubectl --context "k3d-${K3D_CLUSTER_NAME}" create namespace "$namespace" 2>&1)
     if  [[ "$out" == *"AlreadyExists"* ]]; then
       echo "Namespace $namespace already exists, skipping"
     else
